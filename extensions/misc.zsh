a52983022555503f5582eb643a47c07f081ea9b8
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-19 18:39:45 +0200

    misc: add `function` keyword

--- extensions/misc.zsh
+++ extensions/misc.zsh
@@ -1,23 +1,23 @@
 ### {{
 # Misc
 
-mcd() { 
+function mcd() { 
   mkdir $1; cd $1 
 }
 
-fix_mp3() { 
+function fix_mp3() { 
   find -iname '*.mp3' -print0 | xargs -0 mid3iconv -eCP1251 --remove-v1 
 }
 
-mpg2flv() { 
+function mpg2flv() { 
   ffmpeg -i $1 -ar 22050 -ab 32 -f flv -s 320x240 `echo $1 | awk -F . '{print $1}'`.flv 
 }
 
-svn_ignore() {
+function svn_ignore() {
   svn propedit svn:ignore .
 }
 
-git_grep_rep() {
+function git_grep_rep() {
   git grep $1 -- $(git rev-parse --show-toplevel)
 }
 
@@ -26,7 +26,7 @@ git_grep_rep() {
 # svnid() { svn propset svn:keywords "Author Date Id Revision" $1 }
 
 # say - append a newline to 'print'
-say() {
+function say() {
   print "$1\n"
 }
 
