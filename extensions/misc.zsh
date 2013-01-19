13596d8494970c4b4e7e5747f59496dd3e65e608
Taras Yagniuk <mrtaryk@gmail.com>
2013-01-19 18:42:08 +0200

    misc: converts a number between different numbering systems: bin2oct, bin2dec, bin2hex, oct2bin, oct2dec, oct2hex, dec2bin, dec2oct, dec2hex, hex2bin, hex2oct, hex2dec

--- extensions/misc.zsh
+++ extensions/misc.zsh
@@ -30,4 +30,42 @@ function say() {
   print "$1\n"
 }
 
+function baseconvert() {
+  if [[ -n "$1" ]] && [[ -n "$2" ]] && [[ -n "$3" ]]; then
+      echo "ibase=${1}; obase=${2}; ${3}" | bc
+  else
+      echo "Usage: $0 <ibase> <obase> <number>"
+      return 1
+  fi
+}
+
+function bin2oct() { echo "ibase=2; obase=8; $@"   | bc }
+
+function bin2dec() { echo "ibase=2; obase=10; $@"  | bc }
+
+function bin2hex() { echo "ibase=2; obase=16; $@"  | bc }
+
+function oct2bin() { echo "ibase=8; obase=2; $@"   | bc }
+
+function oct2dec() { echo "ibase=8; obase=10; $@"  | bc }
+
+function oct2hex() { echo "ibase=8; obase=16; $@"  | bc }
+
+function dec2bin() { echo "ibase=10; obase=2; $@"  | bc }
+
+function dec2oct() { echo "ibase=10; obase=8; $@"  | bc }
+
+function dec2hex() { echo "ibase=10; obase=16; $@" | bc }
+
+function hex2bin() { echo "ibase=16; obase=2; $@"  | bc }
+
+function hex2oct() { echo "ibase=16; obase=8; $@"  | bc }
+
+function hex2dec() { echo "ibase=16; obase=10; $@" | bc }
+
+
+function hex2dec_f() { printf "%d\n" "0x$@" }
+
+function dec2hex_f() { printf "%x\n" "$@"   }
+
 ### }}
